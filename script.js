42e6701896a4181f4f172f0e9f26236f2deb31e8
vikas <rawatvikas689@gmail.com>
2024-09-12 00:56:07 +0530

    commit

--- script.js
+++ script.js
@@ -1,21 +1,85 @@
-function textAnimation() {
-  var tl = gsap.timeline();
-  tl.to("#loader", {
-    opacity: 0,
-    duration: 0.2,
-    delay: 0.7,
+gsap.registerPlugin(ScrollTrigger);
+function loco() {
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
   });
 
-  tl.to("#loader", {
-    display: "none",
-  });
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+
+function textAnimation() {
+  var tl = gsap.timeline();
+  // tl.to("#loader", {
+  //   opacity: 0,
+  //   duration: 0.2,
+  //   delay: 0.7,
+  // });
+
+  // tl.to("#loader", {
+  //   display: "none",
+  // });
 
   gsap.from(".line h1", {
     y: 100,
     stagger: 0.25,
     duration: 1,
-    delay: 0.1,
+    delay: 0.7,
   });
 }
-textAnimation()
+// loco();
 
+textAnimation();
+
+var tl = gsap.timeline();
+
+// tl.to(".video-text",{
+//   opacity: 0,
+//   duration: 0.1,
+//   delay: 0.1,
+
+//   scrollTrigger: {
+//     // markers: true,
+//     scroller: "body",
+//     start: "10% 40%",
+//     end: "10% 90%",
+//     stagger: 0.4,
+//     scrub:6,
+//     // ease:power,
+//   },
+// })
+
+
+tl.to(".video-extend", {
+  width: "95vw", 
+  scrollTrigger: {
+    markers: true,
+    scroller: "body",
+    start: "10% 30%",
+    end: "10% 80%",
+    stagger: 0.4,
+    scrub: 6,
+  },
+});
