37f7f31725f67183d3e4abbaef849c433e86811a
vikas <rawatvikas689@gmail.com>
2024-09-11 23:34:28 +0530

    commit

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 var tl = gsap.timeline();
-tl.from("#loader",{
-  y:100,
-  opacity: 0,
-  duration: 1,
-})
-tl.from(".main ", {
-  y: 100,
-  stagger:0.25,
-  duration:0.6,
-  delay:0.1,
-});
+// tl.to("#loader", {
+// //   opacity: 0,
+// //   duration: 0.2,
+// //   delay: 0.7,
+// // });
+// tl.from(".main ", {
+//   delay: 0.1,
+//     y: 1600,
+//     duration: 0.5,
+//     ease: Power4,
+// });
+// tl.to("#loader", {
+//   display:"none",
+// });
+// gsap.from(".line h1", {
+//   y: 100,
+//   stagger: 0.25,
+//   duration: 1,
+//   delay: 0.1,
+// });
 
-gsap.from(".line h1", {
-  y: 100,
-  stagger: 0.25,
-  duration: 1,
-  delay: 0.1,
-});
+// setInterval(function () {
+//   let count = 0;
+// }, 1000);
