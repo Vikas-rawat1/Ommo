213d47e722fdc228f79868dff08967dfe8fc450f
vikas <rawatvikas689@gmail.com>
2024-09-11 23:21:44 +0530

    commit

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var tl = gsap.timeline();
-
+tl.from("#loader",{
+  y:100,
+  opacity: 0,
+  duration: 1,
+})
 tl.from(".main ", {
-  y: 10,
+  y: 100,
   stagger:0.25,
   duration:0.6,
-  delay:0.2,
+  delay:0.1,
 });
 
 gsap.from(".line h1", {
