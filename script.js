53ce4efab067d18f0822700ea70468ae4c7db22b
vikas <rawatvikas689@gmail.com>
2024-09-11 23:05:17 +0530

    commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-gsap.to(".heroTextAnimation p", {
-  y: -150,
+var tl = gsap.timeline();
+
+tl.from(".main", {
+  y: 1800,
+  stagger:0.2,
+  // duration:0.1,
+});
+
+tl.from(".line h1 ", {
+  y: 60,
+  stagger: 0.5,
+  duration: 0.6,
+  delay: 0.2,
 });
