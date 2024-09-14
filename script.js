bef40098256005ab581c503ba4a68c66863079f7
vikas <rawatvikas689@gmail.com>
2024-09-14 12:24:06 +0530

    commit

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ function loco() {
 
 var cursor = document.querySelector("#cursor");
 var page2 = document.querySelector(".page2");
-var footer = document.querySelector(".footer-text");
 
 function cursorMovement() {
   window.addEventListener("mousemove", function (dets) {
@@ -70,7 +69,8 @@ function textAnimation() {
     // y: 1000,
     duration: 0.4,
     delay: 0.5,
-  }).to("#loader", {
+  });
+  tl.to("#loader", {
     display: "none",
   });
 
@@ -93,7 +93,7 @@ function textAnimation() {
     delay: 0.2,
     scrollTrigger: {
       // markers: true,
-      scroller: "body",
+      scroller: "#main",
       start: "25% 40%",
       end: "10% 80%",
       stagger: 0.4,
@@ -108,7 +108,7 @@ function textAnimation() {
     scrollTrigger: {
       // markers: true,
       trigger: "testimonial",
-      scroller: "body",
+      scroller: "#main",
       start: "55% 40%",
       end: "60% 80%",
       stagger: 0.4,
@@ -123,7 +123,7 @@ function textAnimation() {
     scrollTrigger: {
       // markers: true,
       trigger: "approachAnimation",
-      scroller: "body",
+      scroller: "#main",
       start: "80% 40%",
       end: "60% 80%",
       stagger: 0.4,
@@ -144,7 +144,7 @@ function videoAnimation() {
 
     scrollTrigger: {
       // markers: true,
-      scroller: "body",
+      scroller: "#main",
       start: "9% 50%",
       end: "10% 90%",
       stagger: 0.2,
@@ -157,7 +157,7 @@ function videoAnimation() {
     width: "95vw",
     scrollTrigger: {
       // markers: true,
-      scroller: "body",
+      scroller: "#main",
       start: "15% 34%",
       end: "10% 80%",
       stagger: 0.4,
@@ -166,45 +166,6 @@ function videoAnimation() {
   });
 }
 
-function footerTextAnimation() {
-  var footerText = document.querySelectorAll(".footer-text p");
-  // var clutter = "";
-  footerText.forEach(function (elem) {
-    // console.log("elem")
-    var clutter = "";
-    var pText = elem.textContent;
-    // console.log(pText);
-    pSplittedText = pText.split("");
-    // console.log(pSplittedText);
-
-    pSplittedText.forEach(function (elem2) {
-      console.log(elem2);
-      clutter += `<span>${elem2}</span>`;
-    });
-    elem.innerHTML = clutter;
-  });
-}
-
-function fotterSinletextAnimation() {
-  var tl = gsap.timeline();
-
-  tl.to(".footer-text p span", {
-    // color: "red",
-    stagger: 0.2,
-    delay: 0.2,
-    duration: 2,
-    scrollTrigger: {
-      trigger: ".footer-text",
-      scroller: "body",
-      start: "10% 70%",
-      end: "30% 50%",
-      markers: true,
-      // stagger: 0.4,
-      scrub: 6,
-    },
-  });
-} 
-
 function page3Animation() {
   gsap.from(".page3", {
     y: 100,
@@ -212,7 +173,7 @@ function page3Animation() {
     delay: 0.1,
     scrollTrigger: {
       trigger: ".page3",
-      scroller: "body",
+      scroller: "#main",
       start: "-40% 50%",
       end: "30% 50%",
       // markers: true,
@@ -228,7 +189,7 @@ function page3Animation() {
     duration: 1,
     scrollTrigger: {
       trigger: ".page3",
-      scroller: "body",
+      scroller: "#main",
       start: "40% 25%",
       end: "30% 50%",
       // markers: true,
@@ -244,10 +205,39 @@ function page3Animation() {
 
 Shery.imageEffect(".left-img", {
   style: 1 /*OR 5 for different variant */,
-  debug: true,
-  // config:{"a":{"value":0,"range":[0,30]},"b":{"value":-0.98,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.1634114980697632},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.09,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
+  // debug: true,
+  config: {
+    a: { value: 0, range: [0, 30] },
+    b: { value: -0.98, range: [-1, 1] },
+    zindex: { value: -9996999, range: [-9999999, 9999999] },
+    aspect: { value: 1.1634114980697632 },
+    ignoreShapeAspect: { value: true },
+    shapePosition: { value: { x: 0, y: 0 } },
+    shapeScale: { value: { x: 0.5, y: 0.5 } },
+    shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+    shapeRadius: { value: 0, range: [0, 2] },
+    currentScroll: { value: 0 },
+    scrollLerp: { value: 0.07 },
+    gooey: { value: false },
+    infiniteGooey: { value: false },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.1, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 0 },
+    noise_speed: { value: 0.2, range: [0, 10] },
+    metaball: { value: 0.2, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0.002, range: [0, 0.1] },
+    noise_height: { value: 0.5, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
 });
-
 
 cursorMovement();
 
