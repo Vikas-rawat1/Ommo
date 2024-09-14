1ccdbdf8557f606515b8f819ae4411ad3200fd61
vikas <rawatvikas689@gmail.com>
2024-09-14 11:50:40 +0530

    commit

--- script.js
+++ script.js
@@ -238,6 +238,12 @@ function page3Animation() {
   });
 }
 
+Shery.imageEffect(".left-img", {
+  style: 1 /*OR 5 for different variant */,
+  debug: true,
+});
+
+
 cursorMovement();
 
 textAnimation();
