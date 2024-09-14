6f905b3f2cee5c94448dfcdb07f91f182b71c4f5
vikas <rawatvikas689@gmail.com>
2024-09-14 12:52:50 +0530

    commit

--- script.js
+++ script.js
@@ -1,37 +1,6 @@
-gsap.registerPlugin(ScrollTrigger);
-// function loco() {
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true,
-  });
-  locoScroll.on("scroll", ScrollTrigger.update);
-
-  ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-    pinType: document.querySelector("#main").style.transform
-      ? "transform"
-      : "fixed",
-  });
-
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-  ScrollTrigger.refresh();
-  return locoScroll;
-
-// }
-
+function locoWork(){
+  
+}
 var cursor = document.querySelector("#cursor");
 var page2 = document.querySelector(".page2");
 
